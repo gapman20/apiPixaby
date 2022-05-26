9fe9059d7a4a3432529a87ace9a96591b8acb227
Gapman20 <jgabriel96@hotmail.com>
2022-05-26 16:59:20 -0500

    Ya funciona el buscador

--- js/main.js
+++ js/main.js
@@ -9,28 +9,36 @@ function getImages(input,key) {
   //console.log(URL);
 }
 
-function render(){
+function getKeyInput(){
     var input = document.getElementById("searchImg").value;
     const key = "27533620-bd97907d4b9fb621b8d6242b4";
     getImages(input,key);
+    document.getElementById("searchImg").value = "";
+    document.getElementsByClassName("cardImg")[0].innerHTML = "";
+    document.getElementsByClassName("buttonPlace")[0].innerHTML = "";
+    
 }
 
 
 function createCard(data) {
   console.log(data);
-  var dataHits = data.hits;  
-  console.log(data.hits);
-  var container = document.getElementsByClassName("cardImg")[0];
-  var element = document.createElement("div");
-  element.classList.add("card");
-  element.innerHTML = `
-
-    <div class="img">
-    <img src="${data.hits}" alt="">
-    </div>
-
-    `;
-
-    container.appendChild(element);
+  
+  for (let index = 0; index < 20; index++) {
+    data.hits[index]
+    
+    console.log(data.hits[index]);
+    var container = document.getElementsByClassName("cardImg")[0];
+    var element = document.createElement("div");
+    element.classList.add("card");
+    element.innerHTML = `
+  
+      <div class="img">
+      <img src="${data.hits[index].previewURL}" alt="">
+      </div>
+  
+      `;
+  
+      container.appendChild(element);
+  }
 }
 
